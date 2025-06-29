fd9c1ca5b07af3ba6e57713ed2586a4d3ebe29e1
nehorosiyparen <sir.kuzin09@yandex.ru>
2025-06-30 02:11:34 +0400

    fliker bug fix

--- frontend/src/app/gates/session.gate.tsx
+++ frontend/src/app/gates/session.gate.tsx
@@ -1,30 +1,29 @@
 import { ReactNode, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { getContainer } from "src/features/auth/di/container";
 import { selectAuthIsLoading } from "src/features/auth/model/selectors";
 import {  useAppSelector } from "src/store/hooks";
+import { publicRoutes } from "../AppRouter/routes/routes.config";
 
 const SessionGate = ({ children }: { children: ReactNode }) => {
     const DIContainer = getContainer()
     const authService = DIContainer.getAuthService()
 
     const navigate = useNavigate()
+    const path = useLocation()
+
+    const [isAuthenticated, ] = useState(async() => {
+        return !!(await authService.authMe())
+    })
 
     const loading = useAppSelector(selectAuthIsLoading)
+    const isPublicRoute = publicRoutes.some(route => route.path === path.pathname)
 
     useEffect(() => {
-        const authenticate = async() => {
-            const response = await authService.authMe()
-
-            console.log(response)
-
-            if (!response) {
+        if (!isAuthenticated && !isPublicRoute) {
                 navigate('/login')
             }
-        }
-        
-        authenticate()
-    }, [])
+    }, [isAuthenticated])
 
     if (loading) return 
 
